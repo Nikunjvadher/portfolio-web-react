988b13e0e55a806270435a6f448673287877141c
Nikunj Vadher <nikunjjv27@gmail.com>
2022-09-12 17:02:35 +0530

    testimonial complted without swiper

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './Testimonial.css'
+import user1 from '../../assests/user1.jpg'
+import user2 from '../../assests/user2.jpg'
+import user3 from '../../assests/user3.jpg'
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+const data = [
+  {
+    user: user1,
+    name: 'Nikunj vadher1',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta debitis facilis voluptatum consectetur molestiae nihil voluptas sit aut itaque hic.'
+  },
+  {
+    user: user2,
+    name: 'Nikunj vadher2',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta debitis facilis voluptatum consectetur molestiae nihil voluptas sit aut itaque hic.'
+  },
+  {
+    user: user3,
+    name: 'Nikunj vadher3',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta debitis facilis voluptatum consectetur molestiae nihil voluptas sit aut itaque hic.'
+  },
+]
 
 const Testimonial = () => {
   return (
-    <section id='testimonial'>Testimonial</section>
+    <section id='testimonials'>
+      
+      <h5>Review From Clients</h5>
+      <h2>Testimonial</h2>
+
+      <div className=" container testimonial__container">
+        {
+          data.map(({user , name , review}, index)=> {
+            return (
+              <article className="testimonial">
+                <div className="clien__img">
+                  <img src={user} alt="Client img" />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review text-light'>{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+
+    </section>
   )
 }
 
